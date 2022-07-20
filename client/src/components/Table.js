7a9d93df5db8d7cddb162cf0ff06d497715c985e
Akbarov90 <ilyasovich706@gmail.com>
2022-07-20 12:22:28 +0500

    adding pagination component

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,19 +1,29 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+import Pagination from "./Pagination";
 
 const Table = () => {
     const [products, setProducts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [productPerPage] = useState(5);
+    const [loading, setLoading] = useState(false);
     // fetching api
     useEffect(() => {
+        setLoading(true);
         axios.get("http://localhost:5000/products/")
             .then((res) => {
                 if (res.data.success)
                     setProducts(res.data.products);
+                setLoading(false);
             })
             .catch((err) => console.log(err))
     }, [])
 
-    console.log(products);
+    const indefOfLastProduct = currentPage * productPerPage;
+    const indexOfFirstProduct = indefOfLastProduct - productPerPage;
+    const currentProducts = products.slice(indexOfFirstProduct, indefOfLastProduct)
+    const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
     return (
         <div>
             <table className={'table table-hover table-bordered'}>
@@ -28,18 +38,20 @@ const Table = () => {
                 </thead>
                 <tbody>
 
-                {products?.map((item, index) => (
-                    <tr>
-                        <th>{index + 1}</th>
-                        <td>{item.date}</td>
-                        <td>{item.title}</td>
-                        <td>{item.quantity}</td>
-                        <td>{item.distance}</td>
-                    </tr>
-                ))}
+                {loading ? <h2>Loading.....</h2> :
+                    currentProducts?.map((item, index) => (
+                        <tr>
+                            <th>{index + 1}</th>
+                            <td>{item.date}</td>
+                            <td>{item.title}</td>
+                            <td>{item.quantity}</td>
+                            <td>{item.distance}</td>
+                        </tr>
+                    ))}
 
                 </tbody>
             </table>
+            <Pagination productsPerPage={productPerPage} totalProducts={products.length} paginate={paginate}/>
         </div>
     );
 }
